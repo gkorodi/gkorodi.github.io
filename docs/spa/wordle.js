cea5446bb63badaaa81289c7613aef9a20a6947f
koRóDi "gabe" GÁbOr <gkorodi@gmail.com>
2025-01-30 10:26:51 -0500

    Update wordle.js
    Change "random word" API provider

--- docs/spa/wordle.js
+++ docs/spa/wordle.js
@@ -318,7 +318,7 @@ class Utilities {
     }
 
     static async getARandomWord() {
-        const response = await fetch("https://random-word-api.herokuapp.com/word?length=5");
+        const response = await fetch("https://random-word-api.vercel.app/api?words=1&length=5");
         const jsonData = await response.json();
         return jsonData[0];
     }
